e23fa7aacbede10b153c07745dcbfe8a3550abbe
Moraa-107 <ok1072005@gmail.com>
2026-05-22 16:49:10 +0300

    The main menu

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << "Classic Cipher Toolkit" << endl;
+
+    cout << "Choose the cipher you want to use:" << endl;
+
+    cout << "1. Caesar Cipher" << endl;
+    cout << "2. Vigenere Cipher" << endl;
+    cout << "3. Playfair Cipher" << endl;
+    cout << "4. Caesar Brute Force" << endl;
+    cout << "5. Exit" << endl;
+    return 0;
+}
