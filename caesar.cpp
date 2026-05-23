eafb09195fc51bb11cddc7622ee2f69173478c5a
Moraa-107 <ok1072005@gmail.com>
2026-05-24 00:29:47 +0300

    Implemented Caesar encryption and decryption

--- caesar.cpp
+++ caesar.cpp
@@ -8,11 +8,32 @@ string encrypt(string plaintext, int shift)
     for (int i = 0; i < plaintext.length(); i++)
     {
         char c = plaintext[i];
-        int pos = c - 'A';
+        int pos = c - 'A'; // h=7 , pos = 7-0=7 , shifted = pos + shift , shift=(3) 7+3=10 10 % 26 =10 -->k
         int shifted = pos + shift;
         shifted = shifted % 26;
         char newChar = shifted + 'A';
         encrypted_text += newChar;
     }
+
     return encrypted_text;
+}
+
+string decrypt(string ciphertext, int shift)
+{
+
+    string decrypted_text = "";
+    for (int i = 0; i < ciphertext.length(); i++)
+    {
+        char c = ciphertext[i]; //  k=10  , pos = 10-0 =10 , shifted = pos- (shift)=3 10 -3 =10 -3 =7 ,  7 % 26 =7 --> h
+        int pos = c - 'A';
+        int shifted = pos - shift;
+        if (shifted < 0)
+        {
+            shifted = shifted + 26;
+        }
+        char newchar = shifted + 'A';
+        decrypted_text += newchar;
+    }
+
+    return decrypted_text;
 }
--- main.cpp
+++ main.cpp
@@ -2,33 +2,54 @@
 #include <string>
 #include "caesar.h"
 using namespace std;
+
 int main()
 {
-
     cout << "Classic Cipher Toolkit" << endl;
-
     cout << "Choose the cipher you want to use:" << endl;
-
     cout << "1. Caesar Cipher" << endl;
     cout << "2. Vigenere Cipher" << endl;
     cout << "3. Playfair Cipher" << endl;
     cout << "4. Caesar Brute Force" << endl;
     cout << "5. Exit" << endl;
-
     cout << "___________________" << endl;
+
     int choice;
     cin >> choice;
-    switch (choice)
 
+    switch (choice)
+    {
     case 1:
     {
-        string plaintext;
-        int shift;
-        cout << "Enter the text you want to encrypt: ";
-        cin >> plaintext;
-        cout << "Enter the shift value: ";
-        cin >> shift;
-        string encrypted_text = encrypt(plaintext, shift);
-        cout << "Encrypted Text: " << encrypted_text << endl;
+        string option;
+        cout << "Encrypt or Decrypt?" << endl;
+        cin >> option;
+
+        if (option == "Encrypt" || option == "encrypt")
+        {
+            string plaintext;
+            int shift;
+            cout << "Enter the text you want to encrypt: ";
+            cin >> plaintext;
+            cout << "Enter the shift value: ";
+            cin >> shift;
+            string encrypted_text = encrypt(plaintext, shift);
+            cout << "Encrypted Text: " << encrypted_text << endl;
+        }
+        else if (option == "Decrypt" || option == "decrypt")
+        {
+            string ciphertext;
+            int shift;
+            cout << "Enter the text you want to decrypt: ";
+            cin >> ciphertext;
+            cout << "Enter the shift value: ";
+            cin >> shift;
+            string decrypted_text = decrypt(ciphertext, shift);
+            cout << "Decrypted Text: " << decrypted_text << endl;
+        }
+        break;
     }
+    }
+
+    return 0;
 }
